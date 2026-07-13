627adf9de29ef22d2ccea06b7e401f3dc148d6c9
Shreeraj Patil <shreerajpatil29@gmail.com>
2026-07-13 13:19:44 +0530

    Update run_tests.groovy to include npm commands
    Added npm install and test commands to run_tests.groovy.

--- vars/run_tests.groovy
+++ vars/run_tests.groovy
@@ -1,6 +1,9 @@
 def call() {
     echo "Running unit tests..."
-    
+    sh '''
+        npm install
+        npm test
+    '''
     // Add your unit test commands here
     // For example:
     // sh "npm test" or "mvn test" depending on your project
