573ea861a6ae37a2e401cba3419d764ac4ea00b7
Shreeraj Patil <shreerajpatil29@gmail.com>
2026-07-13 17:56:41 +0530

    Update update_k8s_manifests.groovy

--- vars/update_k8s_manifests.groovy
+++ vars/update_k8s_manifests.groovy
@@ -40,7 +40,7 @@ def call(Map config = [:]) {
             fi
             
             # Check for changes
-            if git diff --quiet; then
+            if git status --porcelain | grep .; then
                 echo "No changes to commit"
             else
                 # Commit and push changes
@@ -48,7 +48,7 @@ def call(Map config = [:]) {
                 git commit -m "Update image tags to ${imageTag} and ensure correct domain [ci skip]"
                 
                 # Set up credentials for push
-                git remote set-url origin https://\${GIT_USERNAME}:\${GIT_PASSWORD}@github.com/shreerajp555/tws-e-commerce-app.git
+                git remote set-url origin https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/shreerajp555/tws-e-commerce-app.git
                 git push origin HEAD:${gitBranch}
             fi
         """
